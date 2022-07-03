2695ef0dcfa55549a4390204de485400c3452ba7
Matheus Santos <97212094+kain-prog@users.noreply.github.com>
2022-07-03 03:40:54 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 8080;
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     app.use(cors());
     next();
 });
